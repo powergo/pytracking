db7f8d0442d7d1e57c10367d6bdae0131d21ea1c
Barthelemy Dagenais <bart@resulto.ca>
2016-08-02 13:32:44 -0400

    added Django views and utilities. refs #1

--- pytracking/django.py
+++ pytracking/django.py
@@ -1,7 +1,65 @@
-try:
-    from ipware.ip import get_ip
-except ImportError:
-    pass
+from django.conf import settings
+from django.http import (
+    HttpResponseRedirect, Http404, HttpResponse)
+from django.views.generic import View
+from ipware.ip import get_ip
+from pytracking.tracking import (
+    get_configuration, TRACKING_PIXEL, PNG_MIME_TYPE)
+
+
+class TrackingView(View):
+    """TODO
+    """
+
+    def notify_tracking_event(self, tracking_result):
+        pass
+
+    def notify_decoding_error(self, exception):
+        pass
+
+    def get_configuration(self):
+        return get_configuration_from_settings()
+
+
+class ClickTrackingView(TrackingView):
+    """TODO
+    """
+
+    def get(self, request, path):
+        configuration = self.get_configuration()
+
+        try:
+            tracking_result = get_tracking_result(
+                request, path, False, configuration)
+        except Exception as e:
+            self.notify_decoding_error(e)
+            raise Http404
+
+        if not tracking_result.tracked_url:
+            raise Http404
+
+        self.notify_tracking_event(tracking_result)
+
+        return HttpResponseRedirect(tracking_result.tracked_url)
+
+
+class OpenTrackingView(TrackingView):
+    """TODO
+    """
+
+    def get(self, request, path):
+        configuration = self.get_configuration()
+
+        try:
+            tracking_result = get_tracking_result(
+                request, path, False, configuration)
+        except Exception as e:
+            self.notify_decoding_error(e)
+            raise Http404
+
+        self.notify_tracking_event(tracking_result)
+
+        return HttpResponse(TRACKING_PIXEL, content_type=PNG_MIME_TYPE)
 
 
 def get_request_data(request):
@@ -13,3 +71,19 @@ def get_request_data(request):
         "user_agent": user_agent,
         "user_ip": ip
     }
+
+
+def get_configuration_from_settings():
+    """TODO
+    """
+    kwargs = getattr(settings, "PYTRACKING_CONFIGURATION")
+    return get_configuration(None, kwargs)
+
+
+def get_tracking_result(request, path, is_open, configuration=None, **kwargs):
+    """TODO
+    """
+    configuration = get_configuration(configuration, kwargs)
+    request_data = get_request_data(request)
+    return configuration.get_tracking_result(
+        path, request_data, is_open)
